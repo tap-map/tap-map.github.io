00adaccef1b026a7d33196dfbd5c19a8bb92692b
David Fawcett <david.fawcett@gmail.com>
2015-01-24 23:09:29 -0600

    styled popups

--- js/script.js
+++ js/script.js
@@ -1,48 +1,53 @@
 var map = L.map('map').setView([45, -93.2], 11);
 
 		L.control.locate({
-    strings: {
-       title: "Zoom to your current location"
-    }
-    ,locateOptions: {
-              maxZoom: 16
-}}).addTo(map);
+                      strings: {
+                                 title: "Zoom to your current location"
+                               }
+               ,locateOptions: {
+                                 maxZoom: 16
+                               }
+                     }).addTo(map);
 
 var baselayer = new L.StamenTileLayer("toner-lite");
 baselayer.addTo(map);
 
 var beerIcon = L.Icon.extend({
-	options: {
-			iconUrl: 'js/images/beer-24.png',
-			iconRetinaUrl: 'js/images/beer-24@2x.png',
-			iconSize: [24, 24],
-			iconAnchor: [12, 12],
-			popupAnchor: [0, -12]}
-		});
+	                             options: {
+		                                 	iconUrl: 'js/images/beer-24.png',
+			                                iconRetinaUrl: 'js/images/beer-24@2x.png',
+			                                iconSize: [24, 24],
+		                                	iconAnchor: [12, 12],
+	                                		popupAnchor: [0, -12]}
+	                          	});
 
-
+var beerPin = L.MakiMarkers.icon({
+    icon: "beer",
+    color: "#23344c",
+    size: "m"
+});
 
 var tapLayer = new L.GeoJSON.AJAX("js/things.geojson",{
 		    pointToLayer: function (feature, latlng) {
-		    	console.log(feature.properties);
+		    	
 				 var html = '';
-        if (feature.properties.web) {
-            html += '<h3><a href="'+ feature.properties.web + '">' + feature.properties.title + '</a></h3>';
-      } else {
-            html += '<h3>' + feature.properties.title + '</h3>';
-      }
-        if (feature.properties.address) {
-            html += '<p>'+ feature.properties.address + '</p>';
-      }
+               if (feature.properties.web) {
+                      html += '<h3><a href="'+ feature.properties.web + '">' + feature.properties.title + '</a></h3>';
+                                           } 
+               else {
+                      html += '<h3>' + feature.properties.title + '</h3>';
+                    }
+               if (feature.properties.address) {
+                      html += '<p>'+ feature.properties.address + '</p>';
+                                               }
       html += '<div class="put"></div>';
-      var popup = new L.popup().setContent(html);
-      console.log(popup);
+      var popup = new L.popup({closeButton:false}).setContent(html);
+
       var marker = new L.marker(latlng);
-                marker.setIcon(new beerIcon);
-                console.log(marker);
-		        marker.bindPopup(popup);
+      marker.setIcon(beerPin);          
+		  marker.bindPopup(popup);
 	   
-		        return marker;		        
+		  return marker;		        
 		    }
 		});
 
